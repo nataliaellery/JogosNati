1f5e9f9a82a706f3b7327a149359a0c2dd81c1ec
Natalia <nataliaellery@gmail.com>
2018-09-13 20:31:53 -0300

    arrumando index

--- Index.php
+++ Index.php
@@ -1,4 +1,7 @@
 ﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<?php
+ echo ("colocando um php aqui pq meeeu deus");
+?>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="pt" xml:lang="pt">
 	<head>
 		<title>Todas fases de programação</title>
